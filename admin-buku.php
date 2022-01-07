b7994166d4aefc9fe781b94d31ed2aa357798a6e
Anugerah20 <nabilpangestu123@gmail.com>
2022-01-07 21:12:51 +0700

    memperbaiki kesalahan penulisan komentar

--- admin-buku.php
+++ admin-buku.php
@@ -17,7 +17,7 @@ if(isset($_POST["search"])) {
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- Fonts -->
+<!-- Icon -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 <head>
