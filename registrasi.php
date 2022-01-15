73df49ed01fe16d06fffdb1a2d1f55ae02754a98
Anugerah20 <nabilpangestu123@gmail.com>
2022-01-15 15:43:57 +0700

    Membuat form input registrasi

---
+++ registrasi.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Registrasi</title>
+</head>
+<body>
+   <h1>Silahkan Registrasi</h1>
+
+   <form action="" method="POST">
+      <div class="label-input">
+         <label for="username">Username :</label>
+         <input type="text" name="username" id="username">
+
+         <label for="email">Email :</label>
+         <input type="text" name="email" id="email">
+
+         <label for="password">Password :</label>
+         <input type="password" name="password" id="password">
+
+         <label for="password2">Konfirmasi Password :</label>
+         <input type="password2" name="password2" id="password2">
+      </div>
+   </form>
+   
+</body>
+</html>
