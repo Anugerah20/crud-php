4fa6080567acacb64cb94a7841f84605c03d624e
Anugerah20 <nabilpangestu123@gmail.com>
2022-01-15 15:45:15 +0700

    menghapus form email di registrasi

--- registrasi.php
+++ registrasi.php
@@ -14,9 +14,6 @@
          <label for="username">Username :</label>
          <input type="text" name="username" id="username">
 
-         <label for="email">Email :</label>
-         <input type="text" name="email" id="email">
-
          <label for="password">Password :</label>
          <input type="password" name="password" id="password">
 
