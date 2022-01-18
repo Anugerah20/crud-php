5dd5326e4d9d480e75257c123d8af33cc7fd52f0
Anugerah20 <nabilpangestu123@gmail.com>
2022-01-18 19:37:57 +0700

    menambahkan fonts

--- login.php
+++ login.php
@@ -6,6 +6,8 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Halaman Login</title>
+   <!-- Fonts -->
+   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
    <style>
       * {
          font-family: 'arial', sans-serif;
