8f15c2f409994db1bae0890b181a6f1eabcbd1e9
Anugerah20 <nabilpangestu123@gmail.com>
2022-01-19 09:47:15 +0700

    menambahkan popup ketika username berhasil ditambahkan

--- registrasi.php
+++ registrasi.php
@@ -1,6 +1,15 @@
 <?php 
-if(isset($_POST['registrasi'])) {
+require 'functions.php';
 
+if(isset($_POST['register'])) {
+
+   if(registrasi($_POST) > 0) {
+      echo "<script>
+               alert('username berhasil ditambahkan');
+            </script>";
+   } else {
+      mysqli_error($conn);
+   }
 }
 ?>
 
@@ -106,7 +115,7 @@ if(isset($_POST['registrasi'])) {
                <label for="password2" for="password" style="margin-right: 218px;">Konfirmasi</label>
                <input type="password" name="password2" id="password2">
                <div class="btn-register">
-                  <button type="submit" name="registrasi">Register</button>
+                  <button type="submit" name="register">Register</button>
                </div>
                <div class="login">
                   <p>Sudah melakukan registrasi? <a href="login.php">Login</a></p>
