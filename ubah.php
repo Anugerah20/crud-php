ef5cd19a406106625abfaff80fed4cea8af9f66b
Anugerah20 <nabilpangestu123@gmail.com>
2022-01-23 14:32:05 +0700

    mengubah tag html

--- ubah.php
+++ ubah.php
@@ -94,7 +94,7 @@ if (isset($_POST["submit"])) {
          <center>
             <ul>
                <li>
-                  <label for="judul"><span style="margin-left: -248px;">Judul<span></label>
+                  <label for="judul"><span style="margin-left: -248px;">judul<span></label>
                   <input type="text" name="judul" id="judul" required oninvalid="this.setCustomValidity('Judul harus di isi!')" oninput="setCustomValidity()" value="<?= $book["judul"]; ?>">
                </li>
                <li>
