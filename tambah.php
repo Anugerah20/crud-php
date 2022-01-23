468006bb81804994456233e7b23e31878e247980
Anugerah20 <nabilpangestu123@gmail.com>
2022-01-23 15:04:36 +0700

    mengubah tag html

--- tambah.php
+++ tambah.php
@@ -96,7 +96,7 @@ if (isset($_POST["submit"])) {
                   <input type="number" name="halaman" id="halaman">
                </li>
                <li>
-                  <label for="penulis"><span style="margin-left: -235px;">Penulis</span></label>
+                  <label for="penulis"><span style="margin-left: -235px;">penulis</span></label>
                   <input type="text" name="penulis" id="penulis" required oninvalid="this.setCustomValidity('Penulis wajib di isi')" oninput="setCustomValidity()">
                </li>
                <li>
